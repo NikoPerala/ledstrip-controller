9d9019eb9df97b753c1206653b659add54c6f1c6
Niko Perälä <eino.kirves@gmail.com>
2024-03-27 21:51:46 +0200

    Tuusing

--- src/ledstrip.c
+++ src/ledstrip.c
@@ -7,9 +7,15 @@
 #include "button.h"
 
 #define LED_AMOUNT 48
+#define RAINBOW_STEP 65535.0 / LED_AMOUNT
+
+
+
 #define LEDSTRIP_STATES\
     X(STATE_RAINBOW)\
-    X(STATE_STATIC_COLOR)
+    X(STATE_STATIC_COLOR)\
+    X(STATE_OFF)\
+    X(STATE_COUNT)
 
 #define X(name) name,
 typedef enum LedstripStates{
@@ -48,8 +54,9 @@ typedef struct LedController {
     uint8_t color_count;
     uint8_t interval;
     uint8_t send_data : 1;
+    uint8_t set_data : 1;
+    Button button;
 } LedController;
-
 
 typedef struct PinParams {
     volatile uint8_t *port;
@@ -74,21 +81,10 @@ int main(void)
     PORTD |=   (1 << PORTD2);   // Set pullup resistor
 
     millis_init();
-    sei();
 
     cRGB leds[LED_AMOUNT];
 
-    LedController lc = { 
-        .state = STATE_STATIC_COLOR, 
-        .angle = 0.f, 
-        .position = 0, 
-        .color_count = 0,
-        .interval = 17,
-        .send_data = 0
-    };
-
     PinParams button_params = { .port = &PIND, .pin = 2 };
-
     Button button = { 
         .press = BUTTON_PRESS_NONE, 
         .previous_state = 0, 
@@ -97,37 +93,65 @@ int main(void)
         .read_func = read_pin_invert
     };
 
-    uint8_t state = STATE_STATIC_COLOR;
+    LedController lc = { 
+        .state = STATE_STATIC_COLOR, 
+        .angle = 0.f, 
+        .position = 0, 
+        .color_count = 0,
+        .interval = 17,
+        .send_data = 0,
+        .set_data = 1,
+        .button = button
+    };
 
     uint32_t c_time = millis();
     uint32_t p_time = c_time;
 
-    int testval = 0;
 
     int button_pressed = 0; 
     int button_press_type = 0;
+
+    int rainbow_offset = 0;
+
     for(;;)
     {
         c_time = millis();
 
-        button_pressed = read_button(&button, c_time);
+        button_pressed = read_button(&lc.button, c_time);
+        if (button_pressed){
+            button_press_type = get_button_press_type(&lc.button);
+            if (button_press_type == BUTTON_PRESS_LONG){
+                lc.set_data = 1;
+                lc.state++;
+                if (lc.state >= STATE_COUNT - 1) lc.state = 0;
+            }
+        }
 
         switch (lc.state){        
             case STATE_STATIC_COLOR:
-                if (button_pressed){
-                    switch ((button_press_type = get_button_press_type(&button))){
-                        case BUTTON_PRESS_QUICK:
-                            testval = 1;
-                            break;
-                        case BUTTON_PRESS_LONG:
-                            testval = 2;
-                            break;
-                    }
-                    set_all_to_cRGB(leds, colors[testval]);
+                if ((button_pressed && button_press_type == BUTTON_PRESS_QUICK) || lc.set_data){
+                    lc.color_count++;
+                    if (lc.color_count == COLOR_AMOUNT) lc.color_count = 0;
+                    set_all_to_cRGB(leds, colors[lc.color_count]);
                     lc.send_data = 1;
+                    lc.set_data = 0;
                 }
+                break;
+            case STATE_RAINBOW:
+                for (int i = 0; i < LED_AMOUNT; ++i){
+                    leds[i] = HSV_to_ColorRGB(i * RAINBOW_STEP, 0xff, 0xff);
+                }
+                lc.send_data = 1;
+                lc.set_data = 0;
+                break;
+            case STATE_OFF:
+                set_all_to_cRGB(leds, (cRGB) { 0, 0, 0});
+                lc.send_data = 1;
+                break;
         }
-        
+       
+        if (button_pressed && button_press_type == BUTTON_PRESS_DOUBLE) lc.state = STATE_OFF;
+
         if (lc.send_data) {
             ws2812_setleds(leds, LED_AMOUNT);
             lc.send_data = 0;
